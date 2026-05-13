30b935af0a1f0f2029edefc24f5e53b88efb118c
prateek mittal <prateekmittal151500@gmail.com>
2026-05-13 23:54:20 +0530

    imported that new file of gsap code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "./Components/Header-Section/header";
 import Hero from "./Components/Hero-Section/hero";
 import Work from "./Components/Work-Section/work";
+import Script from "./script";
 
 const App = () => {
   return (
     <>
+      <Script />
+
       <Header />
+
       <main>
         <Hero />
         <Work />
