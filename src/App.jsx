f0802bf7375d62d1acfdf177b49d092ab1fd5cad
prateek mittal <prateekmittal151500@gmail.com>
2026-05-14 00:40:43 +0530

    Installed vercel analytics and impliment it.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Header from "./Components/Header-Section/header";
 import Hero from "./Components/Hero-Section/hero";
 import Work from "./Components/Work-Section/work";
 import Script from "./script";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <>
+      <Analytics />
+
       <Script />
 
       <Header />
