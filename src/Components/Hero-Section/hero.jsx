39bae2859f0b67d8183c1881a50627f4ca64928d
prateek mittal <prateekmittal151500@gmail.com>
2026-05-13 23:53:12 +0530

    Removed gsap code from these files

--- src/Components/Hero-Section/hero.jsx
+++ src/Components/Hero-Section/hero.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 import HeroContent from "./heroContent";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
-  useGSAP(() => {
-    gsap.to(".name", {
-      y: -120,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    });
-  });
-
   return (
     <section className="hero flex ac jc">
       <div className="container">
--- src/Components/Work-Section/work.jsx
+++ src/Components/Work-Section/work.jsx
@@ -3,25 +3,7 @@ import WorkTitle from "./workTitle";
 import WorkCta from "./workCta";
 import WebsitePreview from "./websitePreview";
 
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(ScrollTrigger);
-
 const Work = () => {
-  useGSAP(() => {
-    gsap.from(".work-title", {
-      y: 40,
-      opacity: 0,
-      scrollTrigger: {
-        trigger: ".work-title",
-        start: "top 80%",
-        end: "top 50%",
-        scrub: true,
-      },
-    });
-  }, []);
 
   return (
     <section id="work" className="work">
