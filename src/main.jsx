b622d9f9f7716f4c6ff8a8d5f2a6cc53b802dea0
prateek mittal <prateekmittal151500@gmail.com>
2026-05-20 19:25:56 +0530

    Added Vercel Speed Insight

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <>
     <App />
-)
+    <SpeedInsights />
+  </>,
+);
