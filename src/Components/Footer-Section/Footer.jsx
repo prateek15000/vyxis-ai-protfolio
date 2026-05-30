6cd2d52102e75baac1b23f656cee5391221ba62a
prateek mittal <prateekmittal151500@gmail.com>
2026-05-30 11:16:30 +0530

    Refactor Footer component: replace static content with reusable subcomponents for better maintainability

--- src/Components/Footer-Section/Footer.jsx
+++ src/Components/Footer-Section/Footer.jsx
@@ -1,59 +1,22 @@
 import React from "react";
 import { ArrowDownLeft } from "lucide-react";
+import FooterTop from "./FooterTop";
+import FooterMiddle from "./FooterMiddle";
+import FooterBottom from "./FooterBottom";
 
 const Footer = () => {
+
   return (
     <footer>
       <div className="container">
         <div className="footer-content f-clmn">
-          <div className="foot-text flex ac sb">
-              <p>
-                Let's Work <br /> Together
-              </p>
 
-            <div className="footArrow">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-arrow-down-left-icon lucide-arrow-down-left"
-              >
-                <path d="M17 7 7 17" />
-                <path d="M17 17H7V7" />
-              </svg>
-            </div>
+          <FooterTop />
 
-            <a class="footCta flex ac" href="#">
-              Get in Touch
-            </a>
-          </div>
+          <FooterMiddle />
 
-          <div className="footMiddle flex ac">
-            <a target="_blank" rel="noopener noreferrer" href="mailto:guristacks@gmail.com">guristacks@gmail.com</a>
-            <a target="_blank" rel="noopener noreferrer" href="tel:+917696992194">+91 7696 992194</a>
-          </div>
+          <FooterBottom />
 
-          <div className="footBottom flex ac sb">
-            <div className="reserve flex f-clmn">
-              <small> Version</small>
-              <p>© 2026 Guri Ghumaan. All rights reserved.</p>
-            </div>
-
-            <div className="footLinks flex f-clmn ac sb">
-              <small> Socials</small>
-              <div className="socialLinks flex ac">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/guristacks">LinkedIn</a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/guri_ghumxn/">Instagram</a>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/guristacks">GitHub</a>
-              </div>
-            </div>
-          </div>
 
         </div>
       </div>
