c825a38d74f70886490ba68f4d43275fb6085ef4
prateek mittal <prateekmittal151500@gmail.com>
2026-06-01 17:33:15 +0530

    Add ScndText component with descriptive text for About section

---
+++ src/Components/AboutPrev-Section/ScndText.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ScndText = () => {
+  return (
+    <p className="abouttext">
+      The combination of my passion for design, code & interaction positions me
+      in a unique place in the web design world.
+    </p>
+  );
+};
+
+export default ScndText;
