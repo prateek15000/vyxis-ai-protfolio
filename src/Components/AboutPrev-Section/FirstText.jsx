51d773817c9d3e19ee8f513b5f3bfff31597a007
prateek mittal <prateekmittal151500@gmail.com>
2026-06-01 17:33:09 +0530

    Add FirstText component with introductory text for About section

---
+++ src/Components/AboutPrev-Section/FirstText.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstText = () => {
+  return (
+    <p className="aboutFirsttext">
+      Helping brands to stand out in the digital era. Together we will set the
+      new status quo. No nonsense, always on the cutting edge.
+    </p>
+  );
+};
+
+export default FirstText;
