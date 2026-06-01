aebc43ff106c34ad2a424b0ae02b42b07118f5e2
prateek mittal <prateekmittal151500@gmail.com>
2026-06-01 17:33:02 +0530

    Add AboutPrevContent component with initial structure and layout

---
+++ src/Components/AboutPrev-Section/AboutPrevContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FirstText from "./FirstText";
+import ScndText from "./ScndText";
+import AboutCTA from "./AboutCTA";
+
+const AboutPrevContent = () => {
+  return (
+    <div className="aboutContent flex sb">
+      <FirstText />
+      <ScndText />
+      <AboutCTA />
+    </div>
+  );
+};
+
+export default AboutPrevContent;
