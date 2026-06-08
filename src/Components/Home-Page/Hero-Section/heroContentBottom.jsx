b3384596382aca7da9223422549f12d6f0d51db8
prateek mittal <prateekmittal151500@gmail.com>
2026-06-08 23:06:24 +0530

    Add shortDescRef prop to HeroContentBottom component for improved text referencing

--- src/Components/Home-Page/Hero-Section/heroContentBottom.jsx
+++ src/Components/Home-Page/Hero-Section/heroContentBottom.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-const HeroContentBottom = ({ styles, surnameRef }) => {
+const HeroContentBottom = ({ styles, surnameRef, shortDescRef }) => {
   return (
     <div className={`${styles.lName} ${styles.flex} ${styles.fClmn}`}>
       <div className={styles.smallIntro}>
-        <p>Frontend</p>
-        <p>Web Developer</p>
+        <p ref={shortDescRef} >Frontend <br /> Web Developer </p>
       </div>
       <h1 ref={surnameRef}>GHUMAAN</h1>
     </div>
