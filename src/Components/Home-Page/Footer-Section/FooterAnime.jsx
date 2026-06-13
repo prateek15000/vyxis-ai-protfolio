8dc7edcb035ede6f0196dc40ee4f95f11707371c
prateek mittal <prateekmittal151500@gmail.com>
2026-06-13 14:48:37 +0530

    Refactor FooterAnime component: update GSAP animations for responsive behavior

--- src/Components/Home-Page/Footer-Section/FooterAnime.jsx
+++ src/Components/Home-Page/Footer-Section/FooterAnime.jsx
@@ -5,15 +5,27 @@ import { useGSAP } from "@gsap/react";
 
 const FooterAnime = ({ footBtn }) => {
     useGSAP(() => {
-        gsap.from(footBtn.current, {
-            left: "40%",
-            scrollTrigger: {
-                trigger: "footer",
-                start: "top 50%",
-                end: "top 30%",
-                scrub: 2,
-            }
-        })
+        if (window.innerWidth < 768) {
+            gsap.from(footBtn.current, {
+                left: "30%",
+                scrollTrigger: {
+                    trigger: footBtn.current,
+                    start: "top 100%",
+                    end: "top 80%",
+                    scrub: 2,
+                }
+            })
+        } else {
+            gsap.from(footBtn.current, {
+                left: "60%",
+                scrollTrigger: {
+                    trigger: footBtn.current,
+                    start: "top 110%",
+                    end: "top 80%",
+                    scrub: 2,
+                }
+            })
+        }
     })
 
 
