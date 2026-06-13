01c84f20dba1bc16b335f4c16084dbe63f25b623
prateek mittal <prateekmittal151500@gmail.com>
2026-06-13 14:48:31 +0530

    Add AboutPrevAnime component for scroll-triggered animations

---
+++ src/Components/Home-Page/AboutPrev-Section/AboutPrevAnime.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const AboutPrevAnime = ({ AboutBtn }) => {
+    useGSAP(() => {
+        if (window.innerWidth < 768) {
+            gsap.from(AboutBtn.current, {
+                left: "30%",
+                scrollTrigger: {
+                    trigger: AboutBtn.current,
+                    start: "top 100%",
+                    end: "top 80%",
+                    scrub: 2,
+                },
+            });
+        } else {
+            gsap.from(AboutBtn.current, {
+                left: "60%",
+                scrollTrigger: {
+                    trigger: AboutBtn.current,
+                    start: "top 110%",
+                    end: "top 80%",
+                    scrub: 2,
+                },
+            });
+        }
+
+
+    });
+
+    return null;
+};
+
+export default AboutPrevAnime;
