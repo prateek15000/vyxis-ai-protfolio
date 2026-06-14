32bae4a49171057de8d03d0f0e0f32ec23e99b75
prateek mittal <prateekmittal151500@gmail.com>
2026-06-15 00:45:32 +0530

    Remove AboutPrevAnime component and related animation logic from AboutCTA

--- src/Components/Home-Page/AboutPrev-Section/AboutCTA.jsx
+++ src/Components/Home-Page/AboutPrev-Section/AboutCTA.jsx
@@ -1,21 +1,14 @@
 import { Link } from "react-router-dom";
-import React, { useRef } from "react";
-import AboutPrevAnime from "./AboutPrevAnime";
 
 const AboutCTA = ({ styles }) => {
-
-  const AboutBtn = useRef(null);
-
   return (
     <>
       <Link
-        ref={AboutBtn}
         to="/about"
         className={`${styles.aboutCta} ${styles.flex} ${styles.ac} ${styles.jc}`}
       >
         About Me
       </Link>
-      <AboutPrevAnime AboutBtn={AboutBtn} />
     </>
   );
 };
