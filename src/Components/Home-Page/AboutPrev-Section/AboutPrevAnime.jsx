fa74f778e16574f9d6b11dfb6eb35e9361f3272a
prateek mittal <prateekmittal151500@gmail.com>
2026-06-15 00:05:44 +0530

    Consolidate GSAP animation logic for improved readability

--- src/Components/Home-Page/AboutPrev-Section/AboutPrevAnime.jsx
+++ src/Components/Home-Page/AboutPrev-Section/AboutPrevAnime.jsx
@@ -7,28 +7,20 @@ gsap.registerPlugin(ScrollTrigger);
 
 const AboutPrevAnime = ({ AboutBtn }) => {
   useGSAP(() => {
-    if (window.innerWidth < 768) {
-      gsap.from(AboutBtn.current, {
-        left: "30%",
-        scrollTrigger: {
-          trigger: AboutBtn.current,
-          start: "top 100%",
-          end: "top 80%",
-          scrub: 1,
-        },
-      });
-    } else {
-      gsap.from(AboutBtn.current, {
-        left: "60%",
-        scrollTrigger: {
-          trigger: AboutBtn.current,
-          start: "top 110%",
-          end: "top 80%",
-          scrub: 1,
-        },
-      });
-    }
-  });
+    const tween = gsap.from(AboutBtn.current, {
+      left: window.innerWidth < 768 ? "30%" : "60%",
+      scrollTrigger: {
+        trigger: AboutBtn.current,
+        start: window.innerWidth < 768 ? "top 100%" : "top 110%",
+        end: "top 80%",
+        scrub: 1,
+      },
+    });
+    return () => {
+      tween.scrollTrigger?.kill();
+      tween.kill();
+    };
+  }, []);
 
   return null;
 };
