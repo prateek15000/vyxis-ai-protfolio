3c5d355b2500e91e98047288cc49bbdbee0fef43
prateek mittal <prateekmittal151500@gmail.com>
2026-06-15 00:05:57 +0530

    Consolidate GSAP animation logic for improved readability

--- src/Components/Home-Page/Footer-Section/FooterAnime.jsx
+++ src/Components/Home-Page/Footer-Section/FooterAnime.jsx
@@ -1,35 +1,27 @@
-import React from 'react'
+import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
 const FooterAnime = ({ footBtn }) => {
-    useGSAP(() => {
-        if (window.innerWidth < 768) {
-            gsap.from(footBtn.current, {
-                left: "30%",
-                scrollTrigger: {
-                    trigger: footBtn.current,
-                    start: "top 100%",
-                    end: "top 80%",
-                    scrub: 1,
-                }
-            })
-        } else {
-            gsap.from(footBtn.current, {
-                left: "60%",
-                scrollTrigger: {
-                    trigger: footBtn.current,
-                    start: "top 110%",
-                    end: "top 80%",
-                    scrub: 1,
-                }
-            })
-        }
-    })
+  useGSAP(() => {
+    const tween = gsap.from(footBtn.current, {
+      left: window.innerWidth < 768 ? "30%" : "60%",
+      scrollTrigger: {
+        trigger: footBtn.current,
+        start: window.innerWidth < 768 ? "top 100%" : "top 110%",
+        end: "top 80%",
+        scrub: 1,
+      },
+    });
 
+    return () => {
+      tween.scrollTrigger?.kill();
+      tween.kill();
+    };
+  }, []);
 
-    return null;
-}
+  return null;
+};
 
-export default FooterAnime
+export default FooterAnime;
