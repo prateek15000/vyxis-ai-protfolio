0761b0814702a41d49e024b2f45cbbfc380b8a2f
prateek mittal <prateekmittal151500@gmail.com>
2026-06-15 00:05:25 +0530

    Refactor Script component: remove unused references and streamline scroll handling

--- src/script.jsx
+++ src/script.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useLocation } from "react-router-dom";
@@ -7,20 +7,15 @@ import Lenis from "lenis";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Script() {
-  const lenisRef = useRef(null);
   const location = useLocation();
 
   useEffect(() => {
-    window.history.scrollRestoration = "manual";
-
     const lenis = new Lenis({
       duration: 1.2,
       smoothWheel: true,
       smoothTouch: true,
       touchMultiplier: 2,
     });
-
-    lenisRef.current = lenis;
 
     lenis.on("scroll", ScrollTrigger.update);
 
@@ -29,15 +24,15 @@ export default function Script() {
     };
 
     gsap.ticker.add(update);
-    gsap.ticker.lagSmoothing(0);
-
-    ScrollTrigger.refresh();
 
     return () => {
       gsap.ticker.remove(update);
       lenis.destroy();
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
+  }, []);
+
+  useEffect(() => {
+    ScrollTrigger.refresh();
   }, [location.pathname]);
 
   return null;
