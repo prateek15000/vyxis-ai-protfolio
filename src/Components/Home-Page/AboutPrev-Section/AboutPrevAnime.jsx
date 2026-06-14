84ce852c4665e30eaeb1ce9c08d9dd162cf0f360
prateek mittal <prateekmittal151500@gmail.com>
2026-06-14 18:25:12 +0530

    Refactor AboutPrevAnime component: improve code formatting and readability

--- src/Components/Home-Page/AboutPrev-Section/AboutPrevAnime.jsx
+++ src/Components/Home-Page/AboutPrev-Section/AboutPrevAnime.jsx
@@ -6,33 +6,31 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const AboutPrevAnime = ({ AboutBtn }) => {
-    useGSAP(() => {
-        if (window.innerWidth < 768) {
-            gsap.from(AboutBtn.current, {
-                left: "30%",
-                scrollTrigger: {
-                    trigger: AboutBtn.current,
-                    start: "top 100%",
-                    end: "top 80%",
-                    scrub: 1,
-                },
-            });
-        } else {
-            gsap.from(AboutBtn.current, {
-                left: "60%",
-                scrollTrigger: {
-                    trigger: AboutBtn.current,
-                    start: "top 110%",
-                    end: "top 80%",
-                    scrub: 1,
-                },
-            });
-        }
+  useGSAP(() => {
+    if (window.innerWidth < 768) {
+      gsap.from(AboutBtn.current, {
+        left: "30%",
+        scrollTrigger: {
+          trigger: AboutBtn.current,
+          start: "top 100%",
+          end: "top 80%",
+          scrub: 1,
+        },
+      });
+    } else {
+      gsap.from(AboutBtn.current, {
+        left: "60%",
+        scrollTrigger: {
+          trigger: AboutBtn.current,
+          start: "top 110%",
+          end: "top 80%",
+          scrub: 1,
+        },
+      });
+    }
+  });
 
-
-    });
-
-    return null;
+  return null;
 };
 
 export default AboutPrevAnime;
