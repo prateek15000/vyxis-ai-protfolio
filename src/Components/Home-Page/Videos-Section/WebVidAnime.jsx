6868f21143a395a8c1f75fe5cea5713d5a8ce237
prateek mittal <prateekmittal151500@gmail.com>
2026-06-18 02:08:53 +0530

    Add WebVidAnime component for animated video section

---
+++ src/Components/Home-Page/Videos-Section/WebVidAnime.jsx
@@ -0,0 +1,82 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const WebVidAnime = ({ sectionRef, topRef, bottomRef }) => {
+  useGSAP(
+    () => {
+      if (window.innerWidth > 768) {
+        gsap.fromTo(
+          topRef.current,
+          { x: "0%" },
+          {
+            x: "-65%",
+            ease: "none",
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true,
+            },
+          },
+        );
+
+        gsap.fromTo(
+          bottomRef.current,
+          { x: "-65%" },
+          {
+            x: "0%",
+            ease: "none",
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true,
+            },
+          },
+        );
+      } else{
+        gsap.fromTo(
+          topRef.current,
+          { x: "0%" },
+          {
+            x: "-45%",
+            ease: "none",
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true,
+            },
+          },
+        );
+
+        gsap.fromTo(
+          bottomRef.current,
+          { x: "-45%" },
+          {
+            x: "0%",
+            ease: "none",
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: "top bottom",
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true,
+            },
+          },
+        );
+      }
+    },
+    { scope: sectionRef },
+  );
+
+  return null;
+};
+
+export default WebVidAnime;
