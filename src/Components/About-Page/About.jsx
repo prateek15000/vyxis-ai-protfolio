2088b7876d930f6a9050994ff225faa95ebe2525
prateek mittal <prateekmittal151500@gmail.com>
2026-06-17 23:00:29 +0530

    Include refs and integrate AboutAnime component

--- src/Components/About-Page/About.jsx
+++ src/Components/About-Page/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Helmet } from "react-helmet-async";
 import Header from "../Home-Page/Header-Section/header";
 import Footer from "../Home-Page/Footer-Section/Footer";
@@ -6,8 +6,12 @@ import styles from "./AboutPage.module.css";
 import AboutTop from "./AboutTop";
 import AboutMiddle from "./AboutMiddle";
 import AboutBottom from "./AboutBottom";
+import AboutAnime from "./AboutAnime";
 
 const About = () => {
+  const blackRef = useRef(null);
+  const helloRef = useRef(null);
+
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "Work", path: "#Work" },
@@ -18,19 +22,16 @@ const About = () => {
     <>
       <Helmet>
         <title>About - Guri Ghumaan</title>
-        <link
-          rel="canonical"
-          href="https://guri-ghumaan.vercel.app/about"
-        />
+        <link rel="canonical" href="https://guri-ghumaan.vercel.app/about" />
       </Helmet>
       <Header navLinks={navLinks} />
       <main>
-        <AboutTop styles={styles} />
+        <AboutTop blackRef={blackRef} helloRef={helloRef} styles={styles} />
         <AboutMiddle styles={styles} />
         <AboutBottom styles={styles} />
       </main>
       <Footer />
-      
+      <AboutAnime blackRef={blackRef} helloRef={helloRef} />
     </>
   );
 };
